88db9cf7533f73b5717a47651e10a03e8642804f
BeaGabriela <biiagabirelaa@gmail.com>
2022-06-01 11:32:55 -0300

    projeto

---
+++ TrabalhoJava/Projeto_Roupas/src/controle/ProcessaRelatorio.java
@@ -0,0 +1,20 @@
+package controle;
+
+
+	import javax.swing.JOptionPane;
+
+	import controle.RelatorioDAO;
+
+	public class ProcessaRelatorio {
+		
+		private static RelatorioDAO rd = new RelatorioDAO();
+
+		public static void setRd(String dados, String arquivo) {
+			if(rd.saveReport(dados, arquivo)) {
+				JOptionPane.showMessageDialog(null, "Relatório salvo com sucesso.");
+			}else {
+				JOptionPane.showMessageDialog(null, "Erro ao salvar relatório.");
+			}
+		}
+	}
+
